65e20ffd442c1cca6a7d8864b459b96db3a805df
deshapriya <deshapriyakhatua@gmail.com>
2023-08-19 07:26:56 +0530

    first commit

---
+++ FirstProject/src/main/java/in/dk/servelet/Home.java
@@ -0,0 +1,23 @@
+package in.dk.servelet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Home extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out = resp.getWriter();
+		out.println("hi welcome again ... s");
+		out.close();
+	
+	}
+}
