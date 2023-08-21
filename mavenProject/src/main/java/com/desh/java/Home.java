caf41539aa8e2e370277a4823639ac66f9cb154a
deshapriya <deshapriyakhatua@gmail.com>
2023-08-21 12:01:58 +0530

    Json response

--- mavenProject/src/main/java/com/desh/java/Home.java
+++ mavenProject/src/main/java/com/desh/java/Home.java
@@ -1,37 +1,37 @@
 package com.desh.java;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.google.gson.Gson;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Home
- */
 public class Home extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
     public Home() {
-        // TODO Auto-generated constructor stub
+        
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		Employee employee = new Employee(1, "Karan", "IT", 5000);
+        String employeeJsonString = new Gson().toJson(employee);
+		PrintWriter out = resp.getWriter();
+		resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        out.print(employeeJsonString);
+        out.flush();
+        
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
 	}
 
