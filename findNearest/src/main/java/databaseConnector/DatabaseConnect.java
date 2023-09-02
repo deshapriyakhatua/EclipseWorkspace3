cfdb979ecb64bf01a981bc77e2faccaf2532ce08
deshapriya <deshapriyakhatua@gmail.com>
2023-09-02 12:48:20 +0530

    alsf

--- findNearest/src/main/java/databaseConnector/DatabaseConnect.java
+++ findNearest/src/main/java/databaseConnector/DatabaseConnect.java
@@ -100,7 +100,7 @@ public class DatabaseConnect {
 		
 	}
 
-	public Object logIn(String userId, String password) {
+	public boolean logIn(String userId, String password) {
 		
 		try {
 			// connecting to database
--- findNearest/src/main/java/servlets/Login.java
+++ findNearest/src/main/java/servlets/Login.java
@@ -2,14 +2,17 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.UUID;
 
 import com.google.gson.Gson;
 
 import databaseConnector.DatabaseConnect;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import objects.LoginResponse;
 
 public class Login extends HttpServlet{
 
@@ -23,8 +26,23 @@ public class Login extends HttpServlet{
 		System.out.println(userId+" "+password);
 		DatabaseConnect dbConnect = new DatabaseConnect();
 		
-		Object loginStatus = dbConnect.logIn(userId, password);
-		String registerJsonStatus = new Gson().toJson(loginStatus);
+		boolean loginStatus = dbConnect.logIn(userId, password);
+		LoginResponse loginResponse;
+		
+		if(loginStatus) {
+			
+			String loginToken = UUID.randomUUID().toString();
+			Cookie cookie = new Cookie("loginToken", loginToken);
+			cookie.setMaxAge(24*60*60);
+			resp.addCookie(cookie);
+			loginResponse = new LoginResponse("Login Successful", userId, loginToken);
+			
+		}
+		else {
+			loginResponse = new LoginResponse("Login Failed", "", "");
+		}
+		
+		String registerJsonStatus = new Gson().toJson(loginResponse);
 
 		PrintWriter out = resp.getWriter();
 		resp.setContentType("application/json");
